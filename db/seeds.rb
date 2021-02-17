b49e82c812ebe7cbbcc44b0a741b242d48dbf51a
Mateus Pavao <mateus_e_mateus@hotmail.com>
2021-02-17 19:04:50 +0100

    routes defined

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants'
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "restaurant #{restaurant.id} is created."
+end
+
+puts 'All Done!'
